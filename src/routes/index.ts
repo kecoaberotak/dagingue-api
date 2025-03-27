d585877eb6bedf06e377dd5e226cdc069a62210e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-03-27 14:36:04 +0700

    Create: route for user endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import healthRouter from "./health.routes";
+import usersRoutes from "./user.routes";
 
 const router = Router();
 
 router.use("/health", healthRouter);
+router.use("/api/users", usersRoutes);
 
 export default router;
