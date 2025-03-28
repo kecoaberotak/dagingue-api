c44c30d135e8aa5b556af3a18300c6030f93d8ab
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-03-28 15:07:13 +0700

    Add: logic for auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,44 @@
+import { supabase } from "../config/supabase";
 import { AuthDTO } from "../dto/auth.dto";
 
 export class AuthService {
-  async signIn(dto: AuthDTO) {
-    // authentifikasi dengan Supabase
+  async signUp(data: AuthDTO) {
+    // registrasi dengan Supabase
+    const { email, password } = data;
+
+    const { error } = await supabase.auth.signUp({ email, password });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return { message: "Registrasi berhasil, silakan cek email untuk verifikasi." };
   }
 
-  async signUp(dto: AuthDTO) {
-    // registrasi dengan Supabase
+  async signIn(data: AuthDTO) {
+    // authentifikasi dengan Supabase
+    const { email, password } = data;
+
+    const { data: signInData, error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return {
+      message: "Login Berhasil",
+      token: signInData.session?.access_token,
+    };
   }
 
   async signOut() {
     // logika logout
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return { message: "Logout Berhasil" };
   }
 }
