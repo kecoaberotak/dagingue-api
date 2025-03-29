54589dd9c450e914ff76391c3003349e9a0e06f0
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-03-29 20:08:18 +0700

    Edit: verifyToken response

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { supabase } from "../config/supabase";
 
-export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const authHeader = req.headers.authorization;
-    console.log(authHeader); // debug
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       res.status(401).json({
         status: false,
         statusCode: 401,
         message: "Token diperlukan untuk mengakses resource ini",
       });
+      return;
     }
 
     const token = authHeader?.split(" ")[1];
@@ -24,6 +24,7 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
         statusCode: 401,
         message: "Token tidak valid atau sudah kedaluwarsa",
       });
+      return;
     }
 
     // data user disimpan ke request
@@ -35,5 +36,6 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
       statusCode: 500,
       message: "Terjadi kesalahan pada server",
     });
+    return;
   }
 };
