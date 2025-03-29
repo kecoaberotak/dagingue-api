98757e2c4da419e8964a5c3c58ef52f565361af4
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-03-29 18:11:09 +0700

    Add: implementing middleware verifyToken for signOut

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { verifyToken } from "../middlewares/verifyToken";
 
 const router = Router();
 const authController = new AuthController();
 
 router.post("/signup", authController.signUp);
 router.post("/signin", authController.signIn);
-router.post("/signout", authController.signOut);
+router.post("/signout", verifyToken, authController.signOut);
 
 export default router;
