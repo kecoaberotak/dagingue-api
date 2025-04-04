027b59efcdbeae95f7ab97800f337b24cc93ea29
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-04-04 20:56:43 +0700

    Create: route for landingPage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import usersRoutes from "./user.routes";
 import authRoutes from "./auth.routes";
 import bumbuRoutes from "./bumbu.routes";
 import potongRoutes from "./potong.routes";
+import landingPageRoutes from "./landingPage.routes";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/users", usersRoutes);
 router.use("/auth", authRoutes);
 router.use("/api/bumbu", bumbuRoutes);
 router.use("/api/potong", potongRoutes);
+router.use("/api/content", landingPageRoutes);
 
 export default router;
