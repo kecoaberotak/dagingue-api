db9e2bb3ddafbc214756db2000d7264d8247b4fd
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-04-07 14:59:03 +0700

    Edit: delete unused route

--- src/routes/landingPage.routes.ts
+++ src/routes/landingPage.routes.ts
@@ -6,9 +6,7 @@ import { verifyToken } from "../middlewares/verifyToken";
 const router = Router();
 
 router.get("/", LandingPageController.getAll);
-router.get("/:key", LandingPageController.getByKey);
-router.post("/", upload.any(), verifyToken, LandingPageController.create);
-router.put("/:key", upload.any(), verifyToken, LandingPageController.update);
-router.delete("/key", verifyToken, LandingPageController.delete);
+// router.post("/", upload.any(), verifyToken, LandingPageController.create);
+router.put("/", upload.any(), verifyToken, LandingPageController.update);
 
 export default router;
