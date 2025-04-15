6f0c3d73371d3f6bf0e76ba5ba359812e210fa9a
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-04-15 14:14:20 +0700

    Reinstall: pino-pretty

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,18 @@
 import pino from "pino";
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "HH:MM:ss Z",
-      ignore: "pid,hostname",
-    },
-  },
-});
+const isProd = process.env.NODE_ENV === "production";
+
+const logger = isProd
+  ? pino() // ✅ production: gunakan logger default (JSON, tidak pakai pino-pretty)
+  : pino({
+      transport: {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      },
+    });
 
 export default logger;
